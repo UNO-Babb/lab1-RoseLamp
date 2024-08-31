7182448c3776d93e2f7c9b1485ffb0a55335978a
RoseLamp <rose.r.lampman@gmail.com>
2024-08-31 00:54:57 +0000

    tests passed

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,14 +6,17 @@
 def main():
   print("First Program")
   #Say hello
-  
+  print ("Hello")
   #Ask for the user's name
-
+  name=input ("what is your name? ")
   #Use the user's name in the program.
-
+  print("hello " + name)
   #Ask the user for their age.
-
+  age=int(input ("How young are you? ")) 
   #Tell the user what year they were born in.
+  year=2024
+  birth_year= (year-age)
+  print ("you were born in " + str(birth_year))
   #Assume that they have not had their birthday yet this year.
 
 
--- MadLib.py
+++ MadLib.py
@@ -6,9 +6,21 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun=input ("give me a noun ")
+  noun2=input ("give me another noun ")
+  noun3=input ("give me another noun ")
+  verb=input ("give me a verb ")
+  verb2=input ("give me another verb ")
+  verb3=input ("give me another verb ")
+  adverb=input ("give me an adverb please ")
   #Print the story with the user supplied words.
-
+  print ("legend has it that at the end of a rainbow lies a " + noun)
+  print ("they say it contains copious amounts of " + noun2)
+  print ("but it's not so easy, in order to reach it one must " + verb)
+  print ("and one must " + verb2)
+  print ("and do so " + adverb)
+  print ("of course it is also guarded by a " + noun3)
+  print ("so one must " + verb3)
 
 
 #Call the main function if this is the file being run.
